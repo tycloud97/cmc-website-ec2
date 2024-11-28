c7102f13680f6c5b08902c7344b3f8218c266333
Ty <tycloud97@gmail.com>
2024-11-28 20:47:27 +0700

    change to 2025

--- simple-web-server/server.js
+++ simple-web-server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Basic CMC 2024.');
+  res.send('Hello DevOps Basic CMC 2025. Thanks');
 });
 
 app.get('/info', (req, res) => {
